51993fab9b790ed3cc40116440eebe2aec2b081a
Ubuntu <jone@main-compute.1e5g43qbr0qe5ll24tirikkvua.bx.internal.cloudapp.net>
2019-06-06 00:37:46 +0000

    change order of matrix size and cores

--- chapter-4/code10/run_all_rounds.sh
+++ chapter-4/code10/run_all_rounds.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 go build -o go_matrix || exit 1;
 final_message="Ended execution. Following times:"
 step_file_header="sample_number,cores,matrix_size,time"
 date=`date '+%Y-%m-%d-%X'`
-#for cores in {1..2}
-for cores in {1..4};
+#for matrix_size in "1000";
+for matrix_size in "100" "1000" "5000" "10000" "25000" "50000" "90000";
 do
-	#for matrix_size in "100";
-	for matrix_size in "100", "1000", "5000", "10000", "25000", "50000", "90000";
+	#for cores in {1..2}
+	for cores in "1" "2" "4";
 	do
 		rm execution-$cores-$matrix_size.csv
 		echo "Step: number of cores is $cores size of matrix is ${matrix_size}x${matrix_size}"
@@ -24,6 +25,7 @@ do
 			step_file_content="$step,$cores,$matrix_size,$sample"
 			echo $step_file_content >> execution-$cores-$matrix_size.csv
 		done
+		echo "Sending email"
 		echo $final_message | mutt -s "[TCC][$date][Cores $cores][Matrix $matrix_size] Relatório execução multiplicação de matrizes go" -a execution-$cores-$matrix_size.csv -- joaoluisguio@gmail.com
 	done
 done
