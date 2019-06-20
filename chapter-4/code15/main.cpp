320c58905213d4de7f91bb7025c9ca6d99796d5b
João Guio <joaoluisguio@gmail.com>
2019-06-20 19:15:43 +0000

    c++ with branches

---
+++ chapter-4/code15/main.cpp
@@ -0,0 +1,73 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <thread>
+
+void MultiplyMatrix(int **A, int **B, int **C, int n, int threads, int id) {
+	for (int i = id; i < n; i+=threads) {
+		for (int k = 0; k < n; k++) {
+			for (int j = 0; j < n; j++) {
+				C[i][j] = C[i][j] + A[i][k] * B[k][j];
+			}
+		}
+	}
+}
+
+void InitMatrix(int **A, int n, bool random) {
+	for (int i = 0; i < n; i++) {
+        A[i] = (int*) malloc(sizeof(int) * n);
+		for (int j = 0; j < n; j++) {
+            		if (random) {
+                		A[i][j] = rand() % 10000;
+            		} else {
+                		A[i][j] = 0;
+            		}
+		}
+	}
+}
+
+void PrintMatrix(int **A, int n) {
+    for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+            std::cout << A[i][j] << ",";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL));
+	if (argc < 2) {
+		std::cout << "Usage: " << argv[0] << " <size of matrix> <number of threads>" << std::endl;
+		exit(1);
+	}
+	int matrix_size = atoi(argv[1]);
+	if (matrix_size == 0) {
+		std::cout << "Cannot use 0 as matrix size" << std::endl;
+		exit(0);		
+	}
+    int threads = atoi(argv[2]);
+	if (threads < 1 || threads > 8) {
+		std::cout << "Number of threads must be between 1 and 8" << std::endl;
+		exit(0);		
+	}
+    int **A = (int**) malloc(sizeof(int*) * matrix_size);
+    int **B = (int**) malloc(sizeof(int*) * matrix_size);
+    int **C = (int**) malloc(sizeof(int*) * matrix_size);
+    InitMatrix(A, matrix_size, true);
+    InitMatrix(B, matrix_size, true);
+    InitMatrix(C, matrix_size, false);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    auto ref_threads = new std::thread[matrix_size];
+    for (int i = 0; i < threads; i++) {
+        ref_threads[i] = std::thread(MultiplyMatrix, A, B, C, matrix_size, threads, i);
+    }
+    for (int i = 0; i < threads; i++) {
+        ref_threads[i].join();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> dur = end - start;
+    std::cout << dur.count() / 1000 << std::endl;
+}
---
+++ chapter-4/code15/run_all_rounds.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+clang++ -lpthread --std=c++11 main.cpp -o c_matrix -Ofast || exit 1;
+final_message="Ended execution. Following times:"
+step_file_header="sample_number,cores,matrix_size,time"
+date=`date '+%Y-%m-%d-%X'`
+#for matrix_size in "1000";
+for matrix_size in "100" "1000" "2000" "3000" "4000" "5000";
+do
+	#for cores in {1..2}
+	for cores in "1" "2" "4";
+	do
+		rm execution-$cores-$matrix_size.csv
+		echo "Step: number of cores is $cores size of matrix is ${matrix_size}x${matrix_size}"
+		final_message="\n\nStep: number of cores is $cores size of matrix is ${matrix_size}x${matrix_size}"
+		echo $step_file_header >> execution-$cores-$matrix_size.csv
+		for step in {1..50};
+		#for step in {1..2};
+		do
+			sample=`./c_matrix $matrix_size $cores`
+			step_file_content="$step,$cores,$matrix_size,$sample"
+			echo $step_file_content >> execution-$cores-$matrix_size.csv
+		done
+		echo "Sending email"
+		echo $final_message | mutt -s "[TCC][$date][Cores $cores][Matrix $matrix_size] Relatório execução multiplicação de matrizes C" -a execution-$cores-$matrix_size.csv -- joaoluisguio@gmail.com
+	done
+done
