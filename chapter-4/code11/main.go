f3878ef6c17fe79e3c795934ca3c533c5e91234b
João Guio <joaoluisguio@gmail.com>
2019-07-08 01:42:42 -0300

    new dining philosopher

--- chapter-4/code11/main.go
+++ chapter-4/code11/main.go
@@ -14,9 +14,9 @@ type philosopher struct {
 }
 
 type fork struct {
-	ID      int
-	dirty   bool
-	Request chan *philosopher
+	ID    int
+	dirty bool
+	Send  chan *fork
 }
 
 var forks []*fork
@@ -25,38 +25,6 @@ var numPhilosophers int
 
 func (p *philosopher) start() {
 	fmt.Println("Starting philosopher", p.ID)
-	useCh := make(chan bool)
-	go func() {
-		canUse := false
-		for {
-			select {
-			case canUse = <-useCh:
-			default:
-				if !canUse {
-					if p.LeftFork != nil && p.LeftFork.dirty {
-						select {
-						case requester := <-p.LeftFork.Request:
-							if p != requester {
-								fmt.Println("Philosopher", p.ID, "gave left fork to philosopher", requester.ID)
-								p.LeftFork = nil
-							}
-						default:
-						}
-					}
-					if p.RightFork != nil && p.RightFork.dirty {
-						select {
-						case requester := <-p.RightFork.Request:
-							if p != requester {
-								fmt.Println("Philosopher", p.ID, "gave right fork to philosopher", requester.ID)
-								p.RightFork = nil
-							}
-						default:
-						}
-					}
-				}
-			}
-		}
-	}()
 	for {
 		// time thinking
 		fmt.Println("Philosopher", p.ID, "is thinking")
@@ -64,20 +32,26 @@ func (p *philosopher) start() {
 		fmt.Println("Philosopher", p.ID, "finished thinking")
 
 		// must eat!
-		for {
-			fmt.Println("Philosopher", p.ID, "is trying to eat")
-			useCh <- true
-			if p.LeftFork != nil && p.RightFork != nil {
-				break
-			}
-			useCh <- false
+		fmt.Println("Philosopher", p.ID, "is trying to eat")
+		select {
+		case p.LeftFork.Send <- p.LeftFork:
+			p.LeftFork = &fork{ID: 0}
+		case p.RightFork.Send <- p.RightFork:
+			p.RightFork = &fork{ID: 0}
+		default:
+		}
+		for p.LeftFork.ID == 0 || p.RightFork.ID == 0 {
 			select {
-			case forks[p.ID-1].Request <- p:
-				p.LeftFork = forks[p.ID-1]
-				p.LeftFork.dirty = false
-			case forks[(p.ID)%numPhilosophers].Request <- p:
-				p.RightFork = forks[(p.ID)%numPhilosophers]
-				p.RightFork.dirty = false
+			case p.LeftFork.Send <- p.LeftFork:
+				p.LeftFork = &fork{ID: 0}
+			case p.RightFork.Send <- p.RightFork:
+				p.RightFork = &fork{ID: 0}
+			case fork := <-forks[p.ID-1].Send:
+				p.LeftFork = fork
+				p.LeftFork.Send = nil
+			case fork := <-forks[p.ID%numPhilosophers].Send:
+				p.RightFork = fork
+				p.RightFork.Send = nil
 			}
 		}
 
@@ -85,12 +59,14 @@ func (p *philosopher) start() {
 		fmt.Println("Philosopher", p.ID, "started eating")
 		time.Sleep(time.Second * 1)
 		fmt.Println("Philosopher", p.ID, "finished eating")
-		p.LeftFork.dirty, p.RightFork.dirty = true, true
-		useCh <- false
+		p.LeftFork.Send = make(chan *fork)
+		p.RightFork.Send = make(chan *fork)
+		fmt.Printf("%+v, %+v, %+v, %+v, %+v\n", gPhilosophers[0], gPhilosophers[1], gPhilosophers[2], gPhilosophers[3], gPhilosophers[4])
+		fmt.Printf("%+v, %+v, %+v, %+v, %+v\n", forks[0], forks[1], forks[2], forks[3], forks[4])
 	}
 }
 
-func initForks(philosophers []*philosopher) {
+func initForksAndPhilosophers(philosophers []*philosopher) {
 	for k, v := range forks {
 		if k%2 == 1 {
 			philosophers[k-1].RightFork = v
@@ -112,14 +88,16 @@ func main() {
 	}
 	philosophers := []*philosopher{}
 	for i := 0; i < numPhilosophers; i++ {
-		forks = append(forks, &fork{ID: i + 1, dirty: true, Request: make(chan *philosopher)})
+		forks = append(forks, &fork{ID: i + 1, dirty: true, Send: make(chan *fork)})
 		philosopher := &philosopher{
-			ID: i + 1,
+			ID:        i + 1,
+			RightFork: &fork{},
+			LeftFork:  &fork{},
 		}
 		philosophers = append(philosophers, philosopher)
 	}
-	initForks(philosophers)
 	gPhilosophers = philosophers
+	initForksAndPhilosophers(philosophers)
 	for _, p := range philosophers {
 		go p.start()
 	}
